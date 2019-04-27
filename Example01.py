42a153977b4b03b1e54b9e466a1d5b3179698827
palomara <palomarangelll@hotmail.com>
2019-04-27 01:56:35 -0300

    first exercises

---
+++ Example01.py
@@ -0,0 +1,3 @@
+print ('O meu primeiro programa em Python')
+print ('O resultado da soma 2+3 é:', 2+3 )
+print('Fim do programa')
---
+++ Funcao_print().py
@@ -0,0 +1,2 @@
+print("Brasil", "ganhou", 5, "titulos mundiais", sep="-")
+print("Brasil", "ganhou", 5, "titulos mundiais", end="")
