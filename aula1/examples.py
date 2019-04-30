aef995d6d09680f7894a265c8e0c05c31684aff7
palomara <palomarangelll@hotmail.com>
2019-04-29 23:28:50 -0300

    organizing

---
+++ aula1/examples.py
@@ -0,0 +1,8 @@
+
+#Dicionário
+cores = {'verde': 'green', 'vermelho': 'red', 'preto': 'black', 'branco': 'white'}
+cor = input('Escolha a cor que deseja traduzir: ').lower()
+traducao = cores.get(cor, 'Esta cor não consta no meu dicionário')
+print(traducao)
+
+
