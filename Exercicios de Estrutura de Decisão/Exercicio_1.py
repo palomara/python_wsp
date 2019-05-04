9da13acc86dac6a4ce1452d60429ae1a2c5ec0d6
gabrieldelfino28 <gabrieldelfino28@gmail.com>
2019-05-04 18:25:55 -0300

    meus exercicios de python

---
+++ Exercicios de Estrutura de Decisão/Exercicio_1.py
@@ -0,0 +1,7 @@
+num1 = int(input("Digite um número: "))
+num2 = int(input("Digite outro número: "))
+
+if (num1 > num2) :
+    print("O número",num1,"é maior que", num2)
+elif(num2 > num1) :
+    print("O número ",num2,"é maior que", num1)
---
+++ If_Else.py
@@ -0,0 +1,9 @@
+idade = int(input("Por favor, nos informe sua idade: ") )
+
+if (idade >= 18) :
+    print("Parabéns, você já tem idade para tirar a carteira.")
+    print("Fim")
+
+elif (idade < 18)  :
+    print("Você não possui idade suficiente.")
+    print("Fim")
